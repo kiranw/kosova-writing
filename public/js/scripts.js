9d34845dfeb9c5e230b72cd023812587ee9b12d7
kiranw <kiranw@mit.edu>
2016-06-26 13:06:18 -0700

    js

---
+++ public/js/scripts.js
@@ -0,0 +1,57 @@
+console.log("test")
+
+// $("#samples").click(
+	// $("#samples").show()
+	// $("header").hide()
+// );
+
+// $("header").click(
+// 	$("#samples").hide()
+// 	$("header").show()
+// );
+function load_samples(){
+	console.log("loading samples")
+    $('#samples').show();
+    $('header').hide()
+}
+
+function load_home(){
+	console.log("loading home")
+    $('#samples').hide();
+    $('header').show();
+}
+
+function load_tutoring(){
+	console.log("loading tutoring")
+    $('#samples').hide();
+    $('header').hide()
+}
+
+function smooth_about(){
+    $.smoothScroll({
+        scrollTarget: $("#about"),
+        easing: 'swing'
+    });
+}
+
+function smooth_tutoring(){
+    $.smoothScroll({
+        scrollTarget: $("#tutoring-anchor"),
+        easing: 'swing'
+    });
+}
+
+
+function smooth_samples(){
+    $.smoothScroll({
+        scrollTarget: $("#samples"),
+        easing: 'swing'
+    });
+}
+
+function smooth_top(){
+    $.smoothScroll({
+        scrollTarget: $("#page-top"),
+        easing: 'swing'
+    });
+}
